0dcc10d68c3a033d810972f7fe7c8dc627083d15
Benjamin Price <price.benjamindaniel@gmail.com>
2018-07-02 22:46:29 -0700

    Created skeleton for using new command line args.

--- estimator.py
+++ estimator.py
@@ -26,6 +26,8 @@ def main(args):
     FOLDS_TO_TRAIN_AGAINST = args.folds
 
     # Assumes default Carvana data folder structure...
+    # TODO: Consider pattern matching as mechanism for selecting
+    # features vs labels?
     IMAGE_DIR = os.path.join(WORKING_DIR, 'train_hq')
     MASK_DIR  = os.path.join(WORKING_DIR, 'train_masks')
 
@@ -57,42 +59,54 @@ def main(args):
     folds = KFolds(IMAGE_FILENAMES, MASK_FILENAMES, 
         num_folds=NUM_FOLDS, sort=False, yield_dict=False)
 
-    # Train separate models on each requested fold.
-    for fold_num in FOLDS_TO_TRAIN_AGAINST:
-        (train_images, train_masks), (eval_images, eval_masks) = folds.get_fold(fold_num)
+    if args.train:
+        # Train separate models on each requested fold.
+        for fold_num in FOLDS_TO_TRAIN_AGAINST:
+            (train_images, train_masks), (eval_images, eval_masks) = folds.get_fold(fold_num)
 
-        # Initialize the Estimator
-        image_segmentor = tf.estimator.Estimator(
-            model_dir='-'.join([MODEL_DIR, str(fold_num)]),
-            model_fn=model_fn,
-            params=params,
-            config=config)
+            # Initialize the Estimator
+            image_segmentor = tf.estimator.Estimator(
+                model_dir='-'.join([MODEL_DIR, str(fold_num)]),
+                model_fn=model_fn,
+                params=params,
+                config=config)
 
-        # Train and evaluate
-        for i in range(NUM_EPOCHS // EPOCHS_BETWEEN_EVALS):
-            print('\nEntering training epoch %d.\n' % (i * EPOCHS_BETWEEN_EVALS))
-            image_segmentor.train(
-                # input_fn is expected to take no arguments
-                input_fn=lambda: input_fn(
-                    train_images,
-                    train_masks,
-                    training=True,
-                    data_format=params['data_format'],
-                    num_repeats=EPOCHS_BETWEEN_EVALS,
-                    batch_size=TRAIN_BATCH_SIZE))
+            # Train and evaluate
+            for i in range(NUM_EPOCHS // EPOCHS_BETWEEN_EVALS):
+                print('\nEntering training epoch %d.\n' % (i * EPOCHS_BETWEEN_EVALS))
+                image_segmentor.train(
+                    # input_fn is expected to take no arguments
+                    input_fn=lambda: input_fn(
+                        train_images,
+                        train_masks,
+                        training=True,
+                        data_format=params['data_format'],
+                        num_repeats=EPOCHS_BETWEEN_EVALS,
+                        batch_size=TRAIN_BATCH_SIZE))
 
-            results = image_segmentor.evaluate(
-                input_fn=lambda: input_fn(
-                    eval_images,
-                    eval_masks,
-                    training=False,
-                    data_format=params['data_format'],
-                    batch_size=EVAL_BATCH_SIZE))
+                results = image_segmentor.evaluate(
+                    input_fn=lambda: input_fn(
+                        eval_images,
+                        eval_masks,
+                        training=False,
+                        data_format=params['data_format'],
+                        batch_size=EVAL_BATCH_SIZE))
 
-            # TODO: Look into writing example images to a tf.summary?
-            print('\nEvaluation results:\n%s\n' % results)
+                # TODO: Look into writing example images to a tf.summary?
+                print('\nEvaluation results:\n%s\n' % results)
+
+    if args.evaluate:
+        # TODO
+        # How should the estimator be loaded in these subsections?
+        # Which model will we use? Does it need to be specified
+        # at the command line?
+        pass
+
+    if args.predict:
+        # TODO
+        pass
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
-    args = arg_parser(sys.argv)
+    args = arg_parser(sys.argv[1:]) # Exclude program name
     main(args)
